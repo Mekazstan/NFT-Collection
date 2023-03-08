036946105c7f7e14f1a8336f6550266395e5c07f
Mekazstan <mekastans@gmail.com>
2023-03-08 08:03:38 -0800

    Newly Added

--- nfTee/constants/index.js
+++ nfTee/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0x7f2b73017DeD93116De87eB0a4B7d319BE843425";
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-delta-five.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
